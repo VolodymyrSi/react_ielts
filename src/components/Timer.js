b199b29f5d6ae4d23251913776e929691c45bbed
VolodymyrSi <volodymyr.sinevych@gmail.com>
2021-10-28 19:50:17 +0300

    updain

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,24 @@
 import clockIcon from '../files/clock-icon.svg';
+import { useEffect, useState } from 'react';
 
 function Timer() {
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => setTimer((timer) => timer + 1), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  let maxTime = 60 * 60;
+  let seconds = 60 - (timer % 60);
+  let minutes = Math.floor(maxTime / 60 - timer / 60);
+
   return (
     <div className="timerContainer">
-        <img src={clockIcon} alt="clockpic" className="clockIcon" />
-        <p>50 minutes left</p>
+      <img src={clockIcon} alt="clockpic" className="clockIcon" />
+      <p>
+        {minutes} : {seconds} minutes left
+      </p>
     </div>
   );
 }
---
+++ src/files/Test_bank.js
@@ -0,0 +1,48 @@
+const taskBank1 = [
+  {
+    book: 10,
+    number: 1,
+    task: 'The first chart below shows how energy is used in an average Australian household. The second chart shows the greenhouse gas emissions which result from this energy use.',
+    img: ''
+  },
+  {
+    book: 10,
+    number: 2,
+    task: 'The tables below give information about sales of Fairtrade*-labelled coffee and bananas in 1999 and 2004 in five European countries.',
+    img: ''
+  },
+  {
+    book: 10,
+    number: 3,
+    task: 'The charts below show what UK graduate and postgraduate students who did not go into full-time work did after leaving college in 2008.',
+    img: ''
+  },
+  {
+    book: 10,
+    number: 4,
+    task: 'The diagrams below show the life cycle of a species of large fish called the salmon. Summarise the information by selecting and reporting the main features, and make comparisons where relevant.',
+    img: ''
+  }
+];
+const taskBank2 = [
+  {
+    book: 10,
+    number: 1,
+    task: 'It is important for children to learn the difference between right and wrong at an early age. Punishment is necessary to help them learn this distinction. To what extent do you agree or disagree with this opinion? What sort of punishment should parents and teachers be allowed to use to teach good behavior to children?'
+  },
+  {
+    book: 10,
+    number: 2,
+    task: 'Some people think that all university students should study whatever they like. Others believe that they should only be allowed to study subjects that will be useful in the future, such as those related to science and technology. Discuss both these views and give your own opinion.'
+  },
+  {
+    book: 10,
+    number: 3,
+    task: 'Countries are becoming more and more similar because people are able to buy the same products anywhere in the world. Do you think this is a positive or negative development?'
+  },
+  {
+    book: 10,
+    number: 4,
+    task: 'Many museums charge for admission while others are free. Do you think the advantages of charging people for admission to museums outweigh the disadvantages?'
+  }
+];
