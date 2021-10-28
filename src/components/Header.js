3ddea04172a68de626ed64c1252076c980890edd
VolodymyrSi <volodymyr.sinevych@gmail.com>
2021-10-28 20:23:06 +0300

    separate finish button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import userIcon from '../files/user-icon.svg';
-import Timer from "./Timer.js";
-
+import Timer from './Timer.js';
+import FinishButton from './Finish_button.js';
 
 function Header() {
   return (
     <div className="header">
-    <div className="headerUserInfoContainer"><img src={userIcon} alt="userpic" className="userIcon" />
-    <p>XXXX-XXXX 123456</p></div>
-      <Timer/>
-      <div className="HeaderButtonContainer">
-        <button className="headerButton">Finish test</button>
+      <div className="headerUserInfoContainer">
+        <img src={userIcon} alt="userpic" className="userIcon" />
+        <p>XXXX-XXXX 123456</p>
+      </div>
+      <Timer />
+      <div>
+        <FinishButton />
         <button className="headerButton">Help</button>
         <button className="headerButton">Hide</button>
       </div>
