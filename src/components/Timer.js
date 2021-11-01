7a1ca66292fd62c4f7cf354bba0626754f04ecb3
VolodymyrSi <volodymyr.sinevych@gmail.com>
2021-11-01 16:48:26 +0200

    add alerts at 5 and 10 minutes

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,5 +1,6 @@
 import clockIcon from '../files/clock-icon.svg';
 import { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
 
 function Timer() {
   const [timer, setTimer] = useState(0);
@@ -13,12 +14,22 @@ function Timer() {
   let seconds = 60 - (timer % 60);
   let minutes = Math.floor(maxTime / 60 - timer / 60);
 
+  function tenMintesLeft() {
+    Swal.fire('10 minutes left');
+  }
+
+  function fiveMinutesLeft() {
+    Swal.fire('5 minutes left');
+  }
+
   return (
     <div className="timerContainer">
       <img src={clockIcon} alt="clockpic" className="clockIcon" />
       <p>
         {minutes} : {seconds} minutes left
       </p>
+      {timer === 60 * 50 && tenMintesLeft()}
+      {timer === 60 * 55 && fiveMinutesLeft()}
     </div>
   );
 }
