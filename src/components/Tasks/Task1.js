f5f83a18ad644d83636a540fa38546d99585e10d
VolodymyrSi <volodymyr.sinevych@gmail.com>
2021-11-26 17:04:09 +0200

    move task header to task 1 / 2 components

--- src/components/Tasks/Task1.js
+++ src/components/Tasks/Task1.js
@@ -8,28 +8,35 @@ import style from '../Task/TaskContainer.module.css';
 
 import { moduleSelector } from '../utils/helper';
 
-
 function Task1() {
-  const {
-    userInput1,
-    setUserInput1,
-    ExamModule,
-    ExamModuleTaskNumber
-  } = useContext(Context);
+  const { userInput1, setUserInput1, ExamModule, ExamModuleTaskNumber } =
+    useContext(Context);
   return (
-    <div className={style.container}>
-      <TaskFile
-        number="1"
-        task={moduleSelector(ExamModule)[0][ExamModuleTaskNumber - 1]['task']}
-        taskNumber={ExamModuleTaskNumber}
-        module={ExamModule}
-      />
-      <TaskForm
-        input={userInput1}
-        setInput={setUserInput1}
-        value={userInput1}
-      />
-    </div>
+    <>
+      <div className="taskWrapper">
+        <div className="taskHeader">
+          <h1>Part 1</h1>
+          <p>
+            You should spend about 20 minutes on this task. Write at least 150
+            words.
+          </p>
+        </div>
+      </div>
+
+      <div className={style.container}>
+        <TaskFile
+          number="1"
+          task={moduleSelector(ExamModule)[0][ExamModuleTaskNumber - 1]['task']}
+          taskNumber={ExamModuleTaskNumber}
+          module={ExamModule}
+        />
+        <TaskForm
+          input={userInput1}
+          setInput={setUserInput1}
+          value={userInput1}
+        />
+      </div>
+    </>
   );
 }
 
--- src/components/Tasks/Task2.js
+++ src/components/Tasks/Task2.js
@@ -11,38 +11,42 @@ import { task2GeneralBank } from '../../files/Test_bank_general.js';
 
 import style from '../Task/TaskContainer.module.css';
 
-
-
-
 function moduleSelector(ExamModule) {
-    let taskBank = null;
-    ExamModule === 'Academic'
-      ? (taskBank = [task1AcademicBank, task2AcademicBank])
-      : (taskBank = [task1GeneralBank, task2GeneralBank]);
-    return taskBank;
-  }
-
+  let taskBank = null;
+  ExamModule === 'Academic'
+    ? (taskBank = [task1AcademicBank, task2AcademicBank])
+    : (taskBank = [task1GeneralBank, task2GeneralBank]);
+  return taskBank;
+}
 
 function Task2() {
-  const {
-    userInput2,
-    setUserInput2,
-    ExamModule,
-    ExamModuleTaskNumber
-  } = useContext(Context);
+  const { userInput2, setUserInput2, ExamModule, ExamModuleTaskNumber } =
+    useContext(Context);
   return (
-    <div className={style.container}>
-      <TaskFile
-        number="2"
-        task={moduleSelector(ExamModule)[1][ExamModuleTaskNumber - 1]['task']}
-        module={ExamModule}
-      />
-      <TaskForm
-        input={userInput2}
-        setInput={setUserInput2}
-        value={userInput2}
-      />
-    </div>
+    <>
+      <div className="taskWrapper">
+        <div className="taskHeader">
+          <h1>Part 2</h1>
+          <p>
+            You should spend about 40 minutes on this task. Write at least 250
+            words.
+          </p>
+        </div>
+      </div>
+
+      <div className={style.container}>
+        <TaskFile
+          number="2"
+          task={moduleSelector(ExamModule)[1][ExamModuleTaskNumber - 1]['task']}
+          module={ExamModule}
+        />
+        <TaskForm
+          input={userInput2}
+          setInput={setUserInput2}
+          value={userInput2}
+        />
+      </div>
+    </>
   );
 }
 
